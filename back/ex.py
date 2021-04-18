bcae99375b6b250024846a1589df38a6df6c1c70
baydex <dex3a.5@gmail.com>
2021-04-18 15:10:43 -0500

    prueba de 2 conexiones

---
+++ back/ex.py
@@ -0,0 +1,10 @@
+import socket
+import selectors
+
+def getIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = str(s.getsockname()[0])
+    s.close()
+    return ip
+print(getIP())
