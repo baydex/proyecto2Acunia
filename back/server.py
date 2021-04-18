2325c3080b0ec1afff346c6d17a2c20bdfc1d044
baydex <dex3a.5@gmail.com>
2021-04-18 16:13:40 -0500

    Nuevo peer y ListServer sin autenticacion

--- back/server.py
+++ back/server.py
@@ -1,10 +1,8 @@
 from _thread import *
 import threading
-
 import socket
 
 print_lock = threading.Lock()
-
 
 def getIP():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,55 +13,31 @@ def getIP():
 
 def threaded(c,ip):
     while True:
-  
-        # data received from client
         data = c.recv(1024)
         if not data:
-            print('Bye')
-              
-            # lock released on exit
+            print('Desconectado',ip)
             print_lock.release()
             break
         print("mensaje ",str(data),"ip:",ip)
-        # reverse the given string from client
         data = data[::-1]
-  
-        # send back reversed string to client
         c.send(data)
-  
-    # connection closed
     c.close()
 
 def Main():
     host = getIP()
-  
-    # reverse a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
-    port = 12345
+    port = 542312
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
-    print("socket binded to port", port)
-    print("socket at host", host)
-  
-    # put the socket into listening mode
+    print("Servidor alojado en", host)
+    print("Con el puerto", port)
     s.listen(5)
-    print("socket is listening")
-  
-    # a forever loop until client wants to exit
+    print("Esperando solicitudes")
     while True:
-  
-        # establish connection with client
         c, addr = s.accept()
-  
-        # lock acquired by client
         print_lock.acquire()
         print('Connected to :', addr[0], ':', addr[1])
-  
-        # Start a new thread and return its identifier
         start_new_thread(threaded, (c,addr[0]))
     s.close()
-  
-  
+
 if __name__ == '__main__':
     Main()
